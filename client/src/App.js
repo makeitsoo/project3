225c87abcf39fe2812a520b5737d160324adf11f
David Cheng <dkcheng10@gmail.com>
2018-04-24 17:07:33 -0700

    created and imported static element/ backend still needs debuggin

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Week from './WeekContainer';
 import Detail from './DetailContainer';
 import LogItButton from './LogItButtonContainer';
 import StatsButton from './StatsButtonContainer';
+import Staticpage from './staticpage';
 import { Switch, Route } from 'react-router';
 
 import './App.css';
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div>
-
+        <Staticpage />
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/stats" component={StatsButton}/>
