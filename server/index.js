d12a4f0c6934593f57917f597ad06bba9fc6c539
enochwu <enochwu7@gmail.com>
2018-04-25 16:10:49 -0700

    Logs
    - Some fancy console logging added.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const app = require('./server');
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`listening on port ${server.address().port}`);
+  console.log('💪🌎 The beast is alive!');
+  console.log(`  👉 Please visit localhost:${server.address().port}`);
 });
