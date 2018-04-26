84f71f75d650b473c07f8c140489fb0450663cbc
enochwu <enochwu7@gmail.com>
2018-04-25 18:46:03 -0700

    Image Links Fixed

--- client/src/staticFooter.jsx
+++ client/src/staticFooter.jsx
@@ -5,10 +5,10 @@ export default class StaticFooter extends React.Component {
     render() {
         return (
           <footer>
-            <img class="NavButton" src="logit.png" width="50px" height="50px" alt="LogIt" />
-            <img class="NavButton" src="history.png" width="50px" height="50px" alt="Workout History" />
-            <img class="NavButton" src="charts.png" width="50px" height="50px" alt="Charts & Stats" />
-            <img class="NavButton" src="logout.png" width="50px" height="50px" alt="Log Out" />
+            <img class="NavButton" src="../assets/images/logit.png" width="100px" height="100px" alt="LogIt" />
+            <img class="NavButton" src="../assets/images/history.png" width="100px" height="100px" alt="Workout History" />
+            <img class="NavButton" src="../assets/images/charts.png" width="100px" height="100px" alt="Charts & Stats" />
+            <img class="NavButton" src="../assets/images/logout.png" width="100px" height="100px" alt="Log Out" />
           </footer>
         );
     }
