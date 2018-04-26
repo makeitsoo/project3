b87a4087be51f70f649a50107370b67c4b644a68
enochwu <enochwu7@gmail.com>
2018-04-25 18:23:37 -0700

    Router Issues fixed.
    STUFF IS DISPLAYING!

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import Week from './WeekContainer';
-import Detail from './DetailContainer';
+// import Week from './WeekContainer';
+// import Detail from './DetailContainer';
 import LogItButton from './LogItButtonContainer';
 import StatsButton from './StatsButtonContainer';
 import Staticpage from './staticpage';
-import { Switch, Route } from 'react-router';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 import StaticFooter from './staticFooter';
 import Home from './DetailContainer';
 
@@ -13,15 +17,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Staticpage />
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/stats" component={StatsButton}/>
-          <Route path="/logit" component={LogItButton}/>
-        </Switch>
-        <StaticFooter />
-      </div>
+      <Router>
+        <div>
+          <Staticpage />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/stats" component={StatsButton}/>
+            <Route path="/logit" component={LogItButton}/>
+          </Switch>
+          <StaticFooter />
+        </div>
+      </Router>
+
     );
   }
 }
