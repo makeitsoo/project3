5e442b2f3a46b5186324cf9688d16653d409c6e1
Barry Williams <barrywilliams84@gmail.com>
2018-04-28 12:27:05 -0700

    react changes and server additions for db paths

---
+++ server/mongoose_Schemas/workoutHistorySchema.js
@@ -0,0 +1,24 @@
+//Using this Schema for MVP. 
+//require Mongoose
+const mongoose = require('mongoose');
+//require Moment
+const moment = require('moment');
+// Define schema
+const Schema = mongoose.Schema;
+
+const NewWorkout = new Schema({
+  date: {
+    type: String,
+    default: moment().format('MM Do YY')
+  },
+  what: String,
+  sets: Number,
+  reps: Number,
+  weight: Number,
+});
+
+// Compile model from schema
+const Workout = mongoose.model('NewWorkout', NewWorkout);
+
+//Export the Workout
+module.exports = Workout;
