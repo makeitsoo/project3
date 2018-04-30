d17415459ea2d51e0e211bec40c7888786e9ad93
Barry Williams <barrywilliams84@gmail.com>
2018-04-29 21:26:06 -0700

    onClick handlers with this.state,renderPage for all Nav buttons in footer - and added a couple actions

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,9 +1,17 @@
 export const actionNames = {
-  changeDate: 'CHANGE_DATE',
+  changePage: 'CHANGE_PAGE',
+  logout: 'LOGOUT',
   requestStarted: 'REQUEST_STARTED',
   requestComplete: 'REQUEST_COMPLETE',
   requestError: 'REQUEST_ERROR',
 };
+
+// export const actionNames = {
+//   changeDate: 'CHANGE_DATE',
+//   requestStarted: 'REQUEST_STARTED',
+//   requestComplete: 'REQUEST_COMPLETE',
+//   requestError: 'REQUEST_ERROR',
+// };
 
 export const changeDateAction = (day) => ({
   type: actionNames.changeDate,
@@ -17,15 +25,23 @@ export const requestStarted = {
 };
 
 
-export const saveCal = (dispatch) => {
-  dispatch(requestStarted);
-  setTimeout(() => {
-    dispatch({
-      type: actionNames.requestComplete,
-    });
-  }, 1000);
-  dispatch(makeRequest({ theSecret: 42 }));
+export function changePageAction (requestBody) {
+  
 };
+
+export function logoutAction () {
+
+};
+
+// export const saveCal = (dispatch) => {
+//   dispatch(requestStarted);
+//   setTimeout(() => {
+//     dispatch({
+//       type: actionNames.requestComplete,
+//     });
+//   }, 1000);
+//   dispatch(makeRequest({ theSecret: 42 }));
+// };
 
 
 export function makeRequest (requestBody) {
--- client/src/staticFooter.jsx
+++ client/src/staticFooter.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from './actions';
 import './App.css';
 
 export default class StaticFooter extends React.Component {
     render() {
         return (
           <footer>
-                <img onClick={this.props.saveCal} id="b1" class="NavButton" src="../assets/images/logit.png" width="100px" height="100px" alt="LogIt" />
-                <img onClick={this.props.saveCal} id="b2" class="NavButton" src="../assets/images/history.png" width="100px" height="100px" alt="Workout History" />
-                <img onClick={this.props.saveCal} id="b3" class="NavButton" src="../assets/images/charts.png" width="100px" height="100px" alt="Charts & Stats" />
-                <img onClick={this.props.saveCal} id="b4" class="NavButton" src="../assets/images/logout.png" width="100px" height="100px" alt="Log Out" />
+                <img onClick={this.state.renderPage} id="b1" class="NavButton" src="../assets/images/logit.png" width="100px" height="100px" alt="LogIt" />
+                <img onClick={this.state.renderPage} id="b2" class="NavButton" src="../assets/images/history.png" width="100px" height="100px" alt="Workout History" />
+                <img onClick={this.state.renderPage} id="b3" class="NavButton" src="../assets/images/charts.png" width="100px" height="100px" alt="Charts & Stats" />
+                <img onClick={this.state.renderPage} id="b4" class="NavButton" src="../assets/images/logout.png" width="100px" height="100px" alt="Log Out" />
           </footer>
         );
     }
