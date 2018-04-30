c78c63add013b189f01e9118ab0d01321f900988
Barry Williams <barrywilliams84@gmail.com>
2018-04-29 20:50:02 -0700

    removed home route from react router since passport just pops up a modal to authenticate user and redirects to stats page. styled stats page and staticfooter buttons

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import LogIt from './components/LogItContain/LogItContain';
 import LogForm from './components/LogForm/LogForm';
 import Stats from './StatsTableContainer';
-import GoogleLogin from './googleLogin';
+//import GoogleLogin from './googleLogin';
 import Staticpage from './staticpage';
 import {
   BrowserRouter as Router,
@@ -19,16 +19,18 @@ class App extends Component {
         <div>
           <Staticpage />
           <Switch>
-            <Route path="/stats" component={Stats}/>
             <Route path="/logit" component={LogForm}/>
-            <Route exact path="/" component={GoogleLogin}/>
+            <Route exact path="/" component={Stats}/>
           </Switch>
           <StaticFooter />
         </div>
       </Router>
-
     );
   }
 }
 
 export default App;
+
+            // don't need this landing page with Passport authentication 
+            // <Route exact path="/" component={GoogleLogin}/>
+
--- client/src/staticFooter.jsx
+++ client/src/staticFooter.jsx
@@ -5,10 +5,10 @@ export default class StaticFooter extends React.Component {
     render() {
         return (
           <footer>
-            <img class="NavButton" src="../assets/images/logit.png" width="100px" height="100px" alt="LogIt" />
-            <img class="NavButton" src="../assets/images/history.png" width="100px" height="100px" alt="Workout History" />
-            <img class="NavButton" src="../assets/images/charts.png" width="100px" height="100px" alt="Charts & Stats" />
-            <img class="NavButton" src="../assets/images/logout.png" width="100px" height="100px" alt="Log Out" />
+                <img onClick={this.props.saveCal} id="b1" class="NavButton" src="../assets/images/logit.png" width="100px" height="100px" alt="LogIt" />
+                <img onClick={this.props.saveCal} id="b2" class="NavButton" src="../assets/images/history.png" width="100px" height="100px" alt="Workout History" />
+                <img onClick={this.props.saveCal} id="b3" class="NavButton" src="../assets/images/charts.png" width="100px" height="100px" alt="Charts & Stats" />
+                <img onClick={this.props.saveCal} id="b4" class="NavButton" src="../assets/images/logout.png" width="100px" height="100px" alt="Log Out" />
           </footer>
         );
     }
