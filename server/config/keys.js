2b55eebc932040c1dfa28bef523703b5d3e3ae4e
Barry Williams <barrywilliams84@gmail.com>
2018-04-30 20:17:51 -0700

    fix for passport login and backend

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	googleClientId: '882732018398-umlr401h2pk82i5g47au7000020f2400.apps.googleusercontent.com',
+	googleClientSecret: 'DQ-uPqzfZuQPKGfoR3Yl1te1',
+	cookieKey: 'f40e3405ktgmkdke053n32m1nNM'
+}
