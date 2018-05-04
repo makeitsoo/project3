0f0ec2733b5b2a121f3aeb6d2987eb1d96d94212
David Cheng <dkcheng10@gmail.com>
2018-05-04 14:29:16 -0700

    table row in progress

--- client/src/StatsTable.jsx
+++ client/src/StatsTable.jsx
@@ -1,29 +1,55 @@
 import React from 'react';
 import TableRow from './TableRow';
+import axios from 'axios';
 
 export default class StatsTable extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: "date",
+			what: "what",
+			sets: 0,
+			reps: 0,
+			weight: 0
+		}
+	}
+
+	componentDidMount() {
+		this.getWorkouts()
+	}
+	
+	getWorkouts = () => {
+		console.log('there')	
+		axios.get('/api/workout')
+			.then((response) => {
+				this.setState({
+					data: response.data
+				});
+			})
+	}
+
   render() {
-    return (
-      <div>
-		<table className="table">
-		  	<thead>
-		    	<tr>
-		      		<th scope="col">Date</th>
-		      		<th scope="col">Exercise</th>
-		      		<th scope="col">Set #</th>
-		      		<th scope="col">Reps</th>
-		      		<th scope="col">Lbs</th>
-		    	</tr>
-		  	</thead>
-			<tbody>
-				{this.props.days.map((eventsThisDay, day) =>
-          			<TableRow key={day}
-          			day={day}
-          			changeDay={this.props.changeDay}
-          			count={eventsThisDay} />)}
-			</tbody>
-		</table>
-      </div>
+			const renderWorkouts = this.state.data ?
+				this.state.data.map((workout, index) => (
+					<TableRow key={index} />
+				)) : "No Workouts"
+		return (
+			<div>
+				<table className="table">
+					<thead>
+						<tr>
+								<th scope="col">Date</th>
+								<th scope="col">Exercise</th>
+								<th scope="col">Set #</th>
+								<th scope="col">Reps</th>
+								<th scope="col">Lbs</th>
+						</tr>
+					</thead>
+					<tbody>
+						{renderWorkouts}
+					</tbody>
+				</table>
+			</div>
     );
   }
 }
--- client/src/TableRow.jsx
+++ client/src/TableRow.jsx
@@ -1,58 +1,12 @@
 import React, { Component } from 'react';
 // import moment from 'moment';
-import _ from 'underscore';
+// import _ from 'underscore';
 
-
-
-export default class TableRow extends Component {
-	 constructor(props) {
-		super(props);
-		this.state = {
-			date: "date",
-			exercise: "exercise",
-			sets: 0,
-			reps: 0,
-			weight: 0
-		}
-	 }
-
-
-	componentDidMount() {
-		fetch('/workout')
-			.then(res => res)
-			.then(function(data) {
-				console.log(data);
-				return data;
-			})
-			.then((date, exercise, sets, reps, weight ) => {
-				console.log("======", date)
-
-			}
-	
-			// this.setState({ 
-			// 	date: date,
-			// 	exercise: exercise,
-			// 	sets: sets,
-			// 	reps: reps,
-			// 	weight: weight
-			// }))	
-			)};	
-
-
+export class TableRow extends React.Component {
 	render() {
-		console.log("state", this.state)
-		return (
-			<div>
-				{_.map(this.state, state => {
-					console.log("map state", state)
-					return (
-						<tr>
-							{/* <td>{date.exercise}</td>	 */}
-						</tr>
-					)
-				})}
-			</div>
-
-		);
+		console.log(this.props);
+		return <div>{this.props.value}</div>;
 	}
 }
+
+export default TableRow;
