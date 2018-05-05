d57f3000353ad17046593131fc0042e4cc81f1fa
Barry Williams <barrywilliams84@gmail.com>
2018-05-04 20:12:01 -0700

    update to date format using moment.js

--- server/models/workout_Schema.js
+++ server/models/workout_Schema.js
@@ -2,17 +2,17 @@
 //require Mongoose
 const mongoose = require('mongoose');
 //require Moment
-// const moment = require('moment');
+const moment = require('moment');
 // Define schema
 const Schema = mongoose.Schema;
 
 const NewWorkout = new Schema({
-  // date: {
-  //   type: String,
-  //   default: moment().format('MM/DD/YY')
-  // },
-  date: { type: Date,
-  default: Date.now },
+  date: {
+    type: String,
+    default: moment().format('MM/DD/YY')
+  },
+  // date: { type: Date,
+  // default: Date.now },
   what: String,
   sets: Number,
   reps: Number,
