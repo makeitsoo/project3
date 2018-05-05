aba764f27818ca6f7e2a3e62450d0a8175986947
Barry Williams <barrywilliams84@gmail.com>
2018-05-04 22:25:20 -0700

    button styling

--- client/src/staticFooter.jsx
+++ client/src/staticFooter.jsx
@@ -7,17 +7,17 @@ import './App.css';
 // change props to state
 
 export default class StaticFooter extends React.Component {
-    changePage() {
-        //  change page to somewhere
+    changePage(event) {
+        // alert('Log Out');
 
     }
     render() {
         return (
           <footer>
             <Link to='/logit'><img onClick={this.changePage} id="b1" className="NavButton" src="../assets/images/logit.png" width="100px" height="100px" alt="LogIt" /></Link>            
-            <Link to='/'><img onClick={this.props.changePage} id="b2" className="NavButton" src="../assets/images/history.png" width="100px" height="100px" alt="Workout History" /></Link>
-            <Link to='/charts'><img onClick={this.props.changePage} id="b3" className="NavButton" src="../assets/images/charts.png" width="100px" height="100px" alt="Charts & Stats" /></Link>
-            <Link to='/logout'><img onClick={this.props.changePage} id="b4" className="NavButton" src="../assets/images/logout.png" width="100px" height="100px" alt="Log Out" /></Link>
+            <Link to='/'><img onClick={this.changePage} id="b2" className="NavButton" src="../assets/images/history.png" width="100px" height="100px" alt="Workout History" /></Link>
+            <Link to='/charts'><img onClick={this.changePage} id="b3" className="NavButton" src="../assets/images/charts.png" width="100px" height="100px" alt="Charts & Stats" /></Link>
+            <Link to='/logout'><img onClick={this.changePage} id="b4" className="NavButton" src="../assets/images/logout.png" width="100px" height="100px" alt="Log Out" /></Link>
           </footer>
         );
     }
