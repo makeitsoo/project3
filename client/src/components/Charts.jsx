682a193d33616ce481ddb9f1525298f7f6ffa09b
Barry Williams <barrywilliams84@gmail.com>
2018-05-06 01:36:07 -0700

    CHARTS

--- client/src/components/Charts.jsx
+++ client/src/components/Charts.jsx
@@ -29,6 +29,33 @@ const data = {
   ]
 };
 
+const data2 = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Avg. # Reps /workout',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [450, 300, 510, 215, 330, 440, 490]
+    }
+  ]
+};
+
 export default class LineGraph extends React.Component {
   // displayName: 'LineExample',
 
@@ -38,6 +65,11 @@ export default class LineGraph extends React.Component {
         <h2>Workouts/week</h2>
         <Line data={data} />
       </div>
+      // ,
+      // <div>
+      //   <h2>Reps /workout</h2>
+      //   <Line data={data2} />
+      // </div>
     );
   }
 };
